4536f6bf0f70f92be79ea575bba536bab0116885
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-08-07 10:24:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const employeeRecords = createEmployeeRecords(employeesData)
 console.log(employeeRecords)*/
 
 function createTimeInEvent (array, timeStamp) {
-    const [date, time] = dateStamp.split(' ');
+    const [date, time] = timeStamp.split(' ');
     array.timeInEvents.push({
         type: "TimeIn",
         hour: parseInt(time, 10),
@@ -44,12 +44,12 @@ function createTimeInEvent (array, timeStamp) {
     timeInEvents: [],
     timeOutEvents: []
   };
-  const dateStamp ="2022-06-01 0600";
-  const updatedEmployeeRecord = createTimeInEvent(employeeRecord, dateStamp);
+  const timeStamp ="2022-06-01 0600";
+  const updatedEmployeeRecord = createTimeInEvent(employeeRecord, timeStamp);
   console.log(updatedEmployeeRecord);*/
 
-  function createTimeOutEvent (array, dateStamp) {
-    const [date,time] = dateStamp.split(' ');
+  function createTimeOutEvent (array, timeStamp) {
+    const [date,time] = timeStamp.split(' ');
     array.timeOutEvents.push({
         type: "TimeOut",
         hour: parseInt(time, 10),
@@ -65,7 +65,7 @@ function createTimeInEvent (array, timeStamp) {
     timeInEvents: [],
     timeOutEvents: []
   };
-  const dateStamp ="2022-06-01 1300";
+  const timeStamp ="2022-06-01 1300";
   const updatedEmployeeRecord = createTimeOutEvent(employeeRecord, date);
   console.log(updatedEmployeeRecord);*/
 
@@ -76,7 +76,7 @@ function createTimeInEvent (array, timeStamp) {
   if (!timeInEvent || !timeOutEvent) {
     throw new Error(`No matching timeInEvent or timeOutEvent found for date: ${date}`);
   }
-  const hoursWorked= (timeOutEvent.hour -timeInEvent.hour);
+  const hoursWorked= (timeOutEvent.hour -timeInEvent.hour)/100;
   return hoursWorked;
     
   }
@@ -91,3 +91,19 @@ function createTimeInEvent (array, timeStamp) {
   const date = '2022-06-01';
   const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
   console.log(hoursWorked);
+
+  function wagesEarnedOnDate(employeeRecord, date) {
+    const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+    return hoursWorked * employeeRecord.payPerHour;
+}
+
+function allWagesFor(employeeRecord) {
+    const allDatesWorked = employeeRecord.timeInEvents.map(event => event.date);
+    const totalWages = allDatesWorked.reduce((total, date) => total + wagesEarnedOnDate(employeeRecord, date), 0);
+    return totalWages;
+}
+
+function calculatePayroll(employeesArray) {
+    return employeesArray.reduce((totalPayroll, employee) => totalPayroll + allWagesFor(employee), 0);
+}
+
